8063d454a5a574b514f06ab90b7be0c94a1f9b98
github-action-benchmark <github@users.noreply.github.com>
2022-12-28 02:31:27 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f3100842dc32a1be58299371f970c10a9bfe0e99

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1672194686303,
+  "repoUrl": "https://github.com/virtuosgames/libplanet",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dev.moreal@gmail.com",
+            "name": "Lee Dogeon",
+            "username": "moreal"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3100842dc32a1be58299371f970c10a9bfe0e99",
+          "message": "Merge pull request #2676 from moreal/correct-log\n\nCorrect logging in `ActionEvaluator`",
+          "timestamp": "2022-12-27T16:21:27+09:00",
+          "tree_id": "f14bea85e4fdc9539b38808afd77fd86434aeb18",
+          "url": "https://github.com/virtuosgames/libplanet/commit/f3100842dc32a1be58299371f970c10a9bfe0e99"
+        },
+        "date": 1672194670811,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Libplanet.Benchmarks.BlockChain.ContainsBlock",
+            "value": 106430.375,
+            "unit": "ns",
+            "range": "± 2073.8089232135153"
+          }
+        ]
+      }
+    ]
+  }
+}
