82bbd8631816cba40a4a48990def5b0e3b4f1262
github-action-benchmark <github@users.noreply.github.com>
2022-12-28 02:33:21 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f3100842dc32a1be58299371f970c10a9bfe0e99

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672194715240,
+  "lastUpdate": 1672194800897,
   "repoUrl": "https://github.com/virtuosgames/libplanet",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -234,6 +234,36 @@ window.BENCHMARK_DATA = {
             "value": 15987.539325842698,
             "unit": "ns",
             "range": "± 1088.343142915578"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev.moreal@gmail.com",
+            "name": "Lee Dogeon",
+            "username": "moreal"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3100842dc32a1be58299371f970c10a9bfe0e99",
+          "message": "Merge pull request #2676 from moreal/correct-log\n\nCorrect logging in `ActionEvaluator`",
+          "timestamp": "2022-12-27T16:21:27+09:00",
+          "tree_id": "f14bea85e4fdc9539b38808afd77fd86434aeb18",
+          "url": "https://github.com/virtuosgames/libplanet/commit/f3100842dc32a1be58299371f970c10a9bfe0e99"
+        },
+        "date": 1672194762886,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Libplanet.Benchmarks.BlockChain.ContainsBlock",
+            "value": 96430,
+            "unit": "ns",
+            "range": "± 2856.5894784298043"
           }
         ]
       }
